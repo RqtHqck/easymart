7f3949fe46f98f90c3adec18207fdaf5be6fc21b
Ihar <151664439+RqtHqck@users.noreply.github.com>
2026-03-11 18:45:17 +0300

    Компонент Footer и стили к нему
    translation.json обновлен
    
    Стили для отрисовки элементов на странице HomePage.tsx

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,38 +1,22 @@
-// import styles from './HomePage.module.scss';
-// import { useTheme } from '@/shared/config';
-// import { useTranslation } from 'react-i18next';
-// import { AppIcon, Button, Input } from '@/shared/ui';
-// import EnFlagIcon from '@/shared/assets/icons/En.svg?react';
-// import Search from '@/shared/assets/icons/Search.svg?react';
+import styles from './HomePage.module.scss';
+import { useTranslation } from 'react-i18next';
 import { Header } from '@/widgets/Header/ui/Header.tsx';
+import { Footer } from '@/widgets/Footer/ui/Footer.tsx';
+import { Link } from 'react-router-dom';
+import { routePaths } from '@/shared/config';
 
 const HomePage = () => {
-  // const { toggleTheme } = useTheme();
-  // const { t, i18n } = useTranslation();
-  //
-  // const changeLanguage = () => {
-  //   i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
-  // };
+  const { t } = useTranslation();
 
   return (
-    <>
+    <div className={styles.pageWrapper}>
       <Header />
-      {/*  <h1 className={styles.title}>{t('hello')}</h1>*/}
-      {/*  <h2 className={styles.title}>Home</h2>*/}
-      {/*  <Input*/}
-      {/*    // type="password"*/}
-      {/*    placeholder="Search..."*/}
-      {/*    rounded*/}
-      {/*    Icon={<AppIcon Icon={Search} size={20} filled={true} />}*/}
-      {/*  />*/}
-      {/*  <Button size="md" theme="primary" onClick={toggleTheme}>*/}
-      {/*    Toggle theme*/}
-      {/*  </Button>*/}
-      {/*  <Button size="xs" theme="tertiary" onClick={changeLanguage}>*/}
-      {/*    <EnFlagIcon />*/}
-      {/*    /!*{i18n.language}*!/*/}
-      {/*  </Button>*/}
-    </>
+      <main className={styles.content}>
+        <h1 className={styles.title}>{t('hello')}</h1>
+        <Link to={routePaths.login}>Login page</Link>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
