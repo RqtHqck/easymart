6e0c1bb5a32d16e03ad4af4c1c583f85ca06b01d
Ihar <151664439+RqtHqck@users.noreply.github.com>
2026-03-12 10:33:56 +0300

    Тесты с vitest и RTL (React Testing Library). Добавить нечего, прсото тестирование компонентов и функций

---
+++ src/shared/config/test/setupTests.ts
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+// import { afterEach } from 'vitest';
+//
+// import {
+//   resetDeterministicCounter,
+//   resetSequences,
+// } from '@/shared/lib/testing/factories/createMockFactories.ts';
+//
+// // Reset mock factory state between tests to ensure deterministic behavior
+// afterEach(() => {
+//   resetDeterministicCounter();
+//   resetSequences();
+// });
